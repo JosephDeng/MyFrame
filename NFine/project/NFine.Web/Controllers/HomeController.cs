e2ae6bec500911ea7f44fe68ff752bb15234c36d
JosephDeng <663281910@qq.com>
2018-03-18 23:57:43 +0800

    提交文件

---
+++ NFine/project/NFine.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿/*******************************************************************************
+ * Copyright © 2016 Framework 版权所有
+ * Author: Joseph
+ * Description: 
+ * Website：
+*********************************************************************************/
+using NFine.Application.SystemManage;
+using NFine.Code;
+using NFine.Domain.Entity.SystemManage;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Mvc;
+
+namespace NFine.Web.Controllers
+{
+    [HandlerLogin]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Default()
+        {
+            return View();
+        }
+       
+    }
+}
